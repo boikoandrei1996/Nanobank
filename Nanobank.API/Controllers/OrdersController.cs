45fea5e10575788118c276349ab70249448ec49f
Andrei Boika <BoikoAndrei1996@gmail.com>
2017-12-11 02:15:41 +0300

    small fix

--- Nanobank.API/Controllers/OrdersController.cs
+++ Nanobank.API/Controllers/OrdersController.cs
@@ -10,7 +10,6 @@ namespace Nanobank.API.Controllers
   [RoutePrefix("api/Orders")]
   public class OrdersController : ApiController
   {
-    [Authorize]
     [Route("")]
     public IHttpActionResult Get()
     {
